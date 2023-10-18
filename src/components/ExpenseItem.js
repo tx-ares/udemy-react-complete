b949c127216037b47d118f398d86238f790abbcc
tx-ares <twinadanz@gmail.com>
2023-10-18 16:19:12 -0500

    add ExpenseItem component and styles

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+    return (<div className="expense-item">
+        <div> October 18th 2023</div>
+        <div className="expense-item__description">
+            <h2> Car Insurance</h2>
+            <div className="expense-item__price">$234.34</div>
+        </div>
+    </div>)
+
+}
+
+export default ExpenseItem;
