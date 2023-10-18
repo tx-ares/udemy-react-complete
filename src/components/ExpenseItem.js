1e1ab6d46a4b137a52dc809fedd45cffa7a4365b
tx-ares <twinadanz@gmail.com>
2023-10-18 16:24:53 -0500

    add dynamic expense data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 10, 18);
+    const expenseTitle = 'Phone Bill'
+    const expenseAmount = '135.50'
+
     return (<div className="expense-item">
-        <div> October 18th 2023</div>
+        <div>{expenseDate.toISOString()}</div>
         <div className="expense-item__description">
-            <h2> Car Insurance</h2>
-            <div className="expense-item__price">$234.34</div>
+            <h2>{expenseTitle}</h2>
+            <div className="expense-item__price">{expenseAmount}</div>
         </div>
     </div>)
 
