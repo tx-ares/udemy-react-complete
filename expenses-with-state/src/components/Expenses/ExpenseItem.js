2ae45fba3d5f978bbeae5940d0ee797fd8491f74
tx-ares <twinadanz@gmail.com>
2023-10-19 11:42:36 -0500

    add state to expense title

--- expenses-with-state/src/components/Expenses/ExpenseItem.js
+++ expenses-with-state/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,22 @@
+import React, { useState } from 'react'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+
+    // useState can take two arguments, first the property name / value, second is the update function.  Conventionally called setMyPropertyName
+    const [title, setTitle] = useState(props.title)
+
     const clickHandler = () => {
-        console.log('Clicked!')
+        setTitle('new title')
     }
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
