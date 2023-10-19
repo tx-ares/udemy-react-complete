236beebbbcac200ae3e3aec05630011dc7cedc1d
tx-ares <twinadanz@gmail.com>
2023-10-19 11:31:17 -0500

    create state exercise project

---
+++ expenses-with-state/src/App.js
@@ -0,0 +1,32 @@
+import Expenses from "./components/Expenses/Expenses";
+
+const App = () => {
+
+  const items = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2023, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Phone Bill',
+      amount: 294.67,
+      date: new Date(2023, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2023, 5, 12),
+    },
+  ];
+
+  return (
+    <div><Expenses items={items} /></div>
+  );
+}
+
+export default App;
