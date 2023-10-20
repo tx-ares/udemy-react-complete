f6e7d599614c3cc28612ee7e0f5478c114bdb76b
tx-ares <twinadanz@gmail.com>
2023-10-20 12:00:31 -0500

    add submit and clear buttons

--- expenses-with-state/src/components/NewExpense/ExpenseForm.js
+++ expenses-with-state/src/components/NewExpense/ExpenseForm.js
@@ -31,23 +31,36 @@ const ExpenseForm = () => {
         })
     }
 
+    const submitHandler = (event) => {
+        event.preventDefault()
+    }
+
+    const clearHandler = () => {
+        setUserInput({
+            title: '',
+            amount: '',
+            date: ''
+        })
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input type="text" value={userInput.title} onChange={titleChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    <input type="number" value={userInput.amount} min="0.01" step="0.01" onChange={amountChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type="date" min="2023-01-01" max="2023-12-31" onChange={dateChangeHandler} />
+                    <input type="date" value={userInput.date} min="2023-01-01" max="2023-12-31" onChange={dateChangeHandler} />
                 </div>
                 <div className='new-expense__actions'>
                     <button type="submit">Add Expense</button>
+                    <button type="button" onClick={clearHandler}>Clear</button>
                 </div>
             </div>
         </form>
