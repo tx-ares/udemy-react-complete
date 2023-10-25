7da795f8714d71eb50876302526b1772c132e0eb
tx-ares <twinadanz@gmail.com>
2023-10-25 12:03:47 -0500

    replace normal jsx elements with styled-component elements

--- styling-concepts/src/components/AuthInputs.jsx
+++ styling-concepts/src/components/AuthInputs.jsx
@@ -8,6 +8,27 @@ const ControlDiv = styled.div`
 	margin-bottom: 1.5rem;
 `;
 
+const Label = styled.label`
+	display: block;
+	margin-bottom: 0.5rem;
+	font-size: 0.75rem;
+	font-weight: 700;
+	letter-spacing: 0.1em;
+	text-transform: uppercase;
+	color: #6b7280;
+`;
+
+const Input = styled.input`
+	width: 100%;
+	padding: 0.75rem 1rem;
+	line-height: 1.5;
+	background-color: #d1d5db;
+	color: #374151;
+	border: 1px solid transparent;
+	border-radius: 0.25rem;
+	box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
 export default function AuthInputs() {
 	const [enteredEmail, setEnteredEmail] = useState('');
 	const [enteredPassword, setEnteredPassword] = useState('');
@@ -32,11 +53,11 @@ export default function AuthInputs() {
 		<div id='auth-inputs'>
 			<ControlDiv>
 				<p>
-					<label
+					<Label
 						className={`label ${emailNotValid ? 'invalid' : ''}`}>
 						Email
-					</label>
-					<input
+					</Label>
+					<Input
 						type='email'
 						className={emailNotValid ? 'invalid' : undefined}
 						onChange={(event) =>
@@ -45,8 +66,8 @@ export default function AuthInputs() {
 					/>
 				</p>
 				<p>
-					<label>Password</label>
-					<input
+					<Label>Password</Label>
+					<Input
 						type='password'
 						className={passwordNotValid ? 'invalid' : undefined}
 						onChange={(event) =>
