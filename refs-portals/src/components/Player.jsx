6e9d799ea9b4ac954afd88704c85ac80fc32ff9c
tx-ares <twinadanz@gmail.com>
2023-10-31 15:18:31 -0500

    add name change logic

--- refs-portals/src/components/Player.jsx
+++ refs-portals/src/components/Player.jsx
@@ -1,11 +1,30 @@
+import { useState } from 'react';
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+	const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+	const [submitted, setSubmitted] = useState(false);
+
+	function handleChange(event) {
+		setSubmitted(false);
+		setEnteredPlayerName(event.target.value);
+	}
+
+	function handleClick(event) {
+		setSubmitted(true);
+	}
+
+	return (
+		<section id='player'>
+			<h2>
+				{submitted ? 'Welcome ' + enteredPlayerName : 'Who are you?'}
+			</h2>
+			<p>
+				<input
+					type='text'
+					onChange={handleChange}
+				/>
+				<button onClick={handleClick}>Set Name</button>
+			</p>
+		</section>
+	);
 }
