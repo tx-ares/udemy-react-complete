34df9cb1cca186ff886142900ddeaca636da94be
tx-ares <twinadanz@gmail.com>
2023-10-31 15:23:06 -0500

    refactor name change handler logic using refs

--- refs-portals/src/components/Player.jsx
+++ refs-portals/src/components/Player.jsx
@@ -1,27 +1,25 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function Player() {
+	const playerName = useRef(); // Ref is similar to using #identifiers in Angular, giving us access to the element itself and its properties
+
 	const [enteredPlayerName, setEnteredPlayerName] = useState(null);
-	const [submitted, setSubmitted] = useState(false);
 
-	function handleChange(event) {
-		setSubmitted(false);
-		setEnteredPlayerName(event.target.value);
-	}
-
-	function handleClick(event) {
-		setSubmitted(true);
+	function handleClick() {
+		setEnteredPlayerName(playerName.current.value);
 	}
 
 	return (
 		<section id='player'>
 			<h2>
-				{submitted ? 'Welcome ' + enteredPlayerName : 'Who are you?'}
+				{enteredPlayerName
+					? 'Welcome ' + enteredPlayerName
+					: 'Who are you?'}
 			</h2>
 			<p>
 				<input
+					ref={playerName}
 					type='text'
-					onChange={handleChange}
 				/>
 				<button onClick={handleClick}>Set Name</button>
 			</p>
