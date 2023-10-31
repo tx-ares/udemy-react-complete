5d1a02fa5a9fddde34277e47ce435639e9579e1c
tx-ares <twinadanz@gmail.com>
2023-10-31 15:54:07 -0500

    fix challenge style, fix timer ref

--- refs-portals/src/App.jsx
+++ refs-portals/src/App.jsx
@@ -1,12 +1,26 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
-  return (
-    <>
-      <Player />
-      <div id="challenges"></div>
-    </>
-  );
+	return (
+		<>
+			<Player />
+			<div id='challenges'>
+				<TimerChallenge
+					title="Don't hurt me"
+					targetTime={1}></TimerChallenge>
+				<TimerChallenge
+					title='Hurt me plenty'
+					targetTime={5}></TimerChallenge>
+				<TimerChallenge
+					title='Ultra Violence'
+					targetTime={10}></TimerChallenge>
+				<TimerChallenge
+					title='Nightmare'
+					targetTime={15}></TimerChallenge>
+			</div>
+		</>
+	);
 }
 
 export default App;
---
+++ refs-portals/src/components/TimerChallenge.jsx
@@ -0,0 +1,40 @@
+import { useRef, useState } from 'react';
+
+export default function TimerChallenge({ title, targetTime }) {
+	const timer = useRef();
+
+	const [timerExpired, setTimerExpired] = useState(false);
+	const [timerStarted, setTimerStarted] = useState(false);
+
+	function handleStart() {
+		timer.current = setTimeout(() => {
+			setTimerExpired(true);
+		}, targetTime * 1000);
+
+		setTimerStarted(true);
+	}
+
+	function handleStop() {
+		clearTimeout(timer.current);
+	}
+
+	return (
+		<section className='challenge'>
+			<h2>{title}</h2>
+			{timerExpired && <p>You lost! Try again.</p>}
+			<p className='challenge-time'>
+				{targetTime} second{targetTime > 1 ? 's' : ''}
+			</p>
+			<p>
+				<button onClick={timerStarted ? handleStop : handleStart}>
+					{timerStarted ? 'Stop' : 'Start'} Challenge
+				</button>
+			</p>
+			<p className={timerStarted && !timerExpired ? 'active' : undefined}>
+				{timerStarted && !timerExpired
+					? 'Tick tock...'
+					: 'Timer inactive'}
+			</p>
+		</section>
+	);
+}
