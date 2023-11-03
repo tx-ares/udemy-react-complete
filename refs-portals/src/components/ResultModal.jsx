a2a5f56fb08268f87b98187eb73700702d16f3ae
tx-ares <twinadanz@gmail.com>
2023-11-03 10:29:28 -0500

    implement portal for dialog jsx to move to modal div in index

--- refs-portals/src/components/ResultModal.jsx
+++ refs-portals/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
 const ResultModal = forwardRef(function ResultModal(
 	{ targetTime, remainingTime, onReset },
@@ -18,7 +19,7 @@ const ResultModal = forwardRef(function ResultModal(
 		};
 	});
 
-	return (
+	return createPortal(
 		<dialog
 			ref={dialog}
 			className='result-modal'
@@ -38,7 +39,8 @@ const ResultModal = forwardRef(function ResultModal(
 				onSubmit={onReset}>
 				<button>Close</button>
 			</form>
-		</dialog>
+		</dialog>,
+		document.getElementById('modal')
 	);
 });
 
