a1ef4f2c656fba923838e2f587ae7cb5d1ecd820
tx-ares <twinadanz@gmail.com>
2023-11-03 10:23:51 -0500

    add score tracker and message to modal

--- refs-portals/src/components/ResultModal.jsx
+++ refs-portals/src/components/ResultModal.jsx
@@ -8,6 +8,7 @@ const ResultModal = forwardRef(function ResultModal(
 
 	const userLost = remainingTime <= 0;
 	const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+	const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
 	useImperativeHandle(ref, () => {
 		return {
@@ -22,6 +23,8 @@ const ResultModal = forwardRef(function ResultModal(
 			ref={dialog}
 			className='result-modal'>
 			{userLost && <h2>You lost!</h2>}
+			{!userLost && <h2>Your score: {score}</h2>}
+
 			<p>
 				The target time was <strong>{targetTime}</strong> seconds.
 			</p>
