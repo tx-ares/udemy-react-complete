9acc77c2269bbd3de57ea62081f6f99738c05687
tx-ares <twinadanz@gmail.com>
2023-11-20 11:34:32 -0600

    refactor tasks list

---
+++ feature-app/src/components/Details/Task.jsx
@@ -0,0 +1,5 @@
+function Task() {
+	return <div className='task flex-col'>Task</div>;
+}
+
+export default Task;
---
+++ feature-app/src/components/Details/TasksList.jsx
@@ -0,0 +1,7 @@
+import Task from './Task';
+
+function TaskList() {
+	return <Task></Task>;
+}
+
+export default TaskList;
