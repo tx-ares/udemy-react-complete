b78835fd0a00903221e500bf78ce090f30b80759
tx-ares <twinadanz@gmail.com>
2023-12-14 16:41:47 -0600

    refactor to use Context Consumer to to access context properties

--- component-composition/src/components/Cart.jsx
+++ component-composition/src/components/Cart.jsx
@@ -3,51 +3,67 @@ import { useContext } from 'react';
 
 export default function Cart({ onUpdateItemQuantity }) {
 	// const cartCtx = useContext(CartContext); // This is fine to use to be specific about the context we are referring to in this component, but means we have to use cartCtx.items.length instead of just items.length
-	const { items } = useContext(CartContext); // Destructuring the items property from the context object means we can use items.length instead of items.length meaning less code to write overall
-
-	const totalPrice = items.reduce(
-		(acc, item) => acc + item.price * item.quantity,
-		0
-	);
-	const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
+	// const { items } = useContext(CartContext); // Destructuring the items property from the context object means we can use items.length instead of items.length meaning less code to write overall
 
 	return (
-		<div id='cart'>
-			{items.length === 0 && <p>No items in cart!</p>}
-			{items.length > 0 && (
-				<ul id='cart-items'>
-					{items.map((item) => {
-						const formattedPrice = `$${item.price.toFixed(2)}`;
+		// Now, we can use the CartContext.Consumer component to access the context data and render the cart items.  This is actually necessary as it now is needing data from the context provider.  provider = pub, consumer = sub
+		<CartContext.Consumer>
+			{(cartContextConsumer) => {
+				const totalPrice = cartContextConsumer.items.reduce(
+					(acc, item) => acc + item.price * item.quantity,
+					0
+				);
+				const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
+				return (
+					<div id='cart'>
+						{cartContextConsumer.items.length === 0 && (
+							<p>No items in cart!</p>
+						)}
+						{cartContextConsumer.items.length > 0 && (
+							<ul id='cart-items'>
+								{cartContextConsumer.items.map((item) => {
+									const formattedPrice = `$${item.price.toFixed(
+										2
+									)}`;
 
-						return (
-							<li key={item.id}>
-								<div>
-									<span>{item.name}</span>
-									<span> ({formattedPrice})</span>
-								</div>
-								<div className='cart-item-actions'>
-									<button
-										onClick={() =>
-											onUpdateItemQuantity(item.id, -1)
-										}>
-										-
-									</button>
-									<span>{item.quantity}</span>
-									<button
-										onClick={() =>
-											onUpdateItemQuantity(item.id, 1)
-										}>
-										+
-									</button>
-								</div>
-							</li>
-						);
-					})}
-				</ul>
-			)}
-			<p id='cart-total-price'>
-				Cart Total: <strong>{formattedTotalPrice}</strong>
-			</p>
-		</div>
+									return (
+										<li key={item.id}>
+											<div>
+												<span>{item.name}</span>
+												<span> ({formattedPrice})</span>
+											</div>
+											<div className='cart-item-actions'>
+												<button
+													onClick={() =>
+														onUpdateItemQuantity(
+															item.id,
+															-1
+														)
+													}>
+													-
+												</button>
+												<span>{item.quantity}</span>
+												<button
+													onClick={() =>
+														onUpdateItemQuantity(
+															item.id,
+															1
+														)
+													}>
+													+
+												</button>
+											</div>
+										</li>
+									);
+								})}
+							</ul>
+						)}
+						<p id='cart-total-price'>
+							Cart Total: <strong>{formattedTotalPrice}</strong>
+						</p>
+					</div>
+				);
+			}}
+		</CartContext.Consumer>
 	);
 }
