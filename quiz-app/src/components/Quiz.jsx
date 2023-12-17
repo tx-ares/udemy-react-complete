bf0a750adb5214356897b742d8e20dd1f98a6121
tx-ares <twinadanz@gmail.com>
2023-12-17 15:01:23 -0600

    create summary component and restart button

--- quiz-app/src/components/Quiz.jsx
+++ quiz-app/src/components/Quiz.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 import QUESTIONS from '../data/questions';
 
@@ -8,33 +9,62 @@ const Quiz = () => {
 
 	const activeQuestionIndex = userAnswers.length;
 
+	const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
 	function handleSelectAnswer(selectedAnswer) {
 		setUserAnswers((prevAnswers) => {
 			return [...prevAnswers, selectedAnswer];
 		});
 	}
 
-	return (
-		<div id='quiz'>
-			<div id='question'>
-				<h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-				<ul id='answers'>
-					{QUESTIONS[activeQuestionIndex].answers.map((answer) => {
-						return (
-							<li
-								key={answer}
-								className='answer'>
-								<button
-									onClick={() => handleSelectAnswer(answer)}>
-									{answer}
-								</button>
-							</li>
-						);
-					})}
-				</ul>
+	let content = '';
+
+	if (quizIsComplete) {
+		return (content = (
+			<div id='summary'>
+				<img
+					src={quizCompleteImg}
+					alt='Trophy'
+				/>
+				<h2>Quiz Complete!</h2>
+				<button
+					className='reset-button'
+					onClick={() => setUserAnswers([])}>
+					Restart Quiz
+				</button>
 			</div>
-		</div>
-	);
+		));
+	} else {
+		const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+		shuffledAnswers.sort(() => Math.random() - 0.5);
+		return (content = (
+			<div id='quiz'>
+				<div id='question'>
+					<h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+					<ul id='answers'>
+						{QUESTIONS[activeQuestionIndex].answers.map(
+							(answer) => {
+								return (
+									<li
+										key={answer}
+										className='answer'>
+										<button
+											onClick={() =>
+												handleSelectAnswer(answer)
+											}>
+											{answer}
+										</button>
+									</li>
+								);
+							}
+						)}
+					</ul>
+				</div>
+			</div>
+		));
+	}
+
+	return { content };
 };
 
 export default Quiz;
