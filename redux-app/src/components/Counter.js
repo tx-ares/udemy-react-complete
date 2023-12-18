1fbf59e70f3c5f9b0b0407d1077462b880c998cd
tx-ares <twinadanz@gmail.com>
2023-12-18 12:05:46 -0600

    Pull counter state from redux store and add Redux store provider

--- redux-app/src/components/Counter.js
+++ redux-app/src/components/Counter.js
@@ -1,15 +1,18 @@
 import classes from './Counter.module.css';
+import { useSelector, connect } from 'react-redux';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+	const counter = useSelector((state) => state.counter); // By using useSelector, we can access a slice of the state. It also creates a subscription from this component to the store. It also removes the subscription upon unmounting ( destroying ) of the component.
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+	const toggleCounterHandler = () => {};
+
+	return (
+		<main className={classes.counter}>
+			<h1>Redux Counter</h1>
+			<div className={classes.value}>{counter}</div>
+			<button onClick={toggleCounterHandler}>Toggle Counter</button>
+		</main>
+	);
 };
 
 export default Counter;
--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import './index.css';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import store from './store/index'; // We must provide a store to the provider component as a prop.
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
