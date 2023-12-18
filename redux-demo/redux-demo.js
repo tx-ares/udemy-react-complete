0dff537550e4507761263cf409020dd6b1884ff9
tx-ares <twinadanz@gmail.com>
2023-12-18 11:44:33 -0600

    Create redux demo to Add Redux dependencies and implement a counter functionality

---
+++ redux-demo/redux-demo.js
@@ -0,0 +1,29 @@
+const redux = require('redux');
+
+// A reducer function is a not a React specific concept.  It is simply a pure function that takes an assortment of values and reduces them to a single value.
+// In the case of Redux, the reducer function takes the current state and an action and returns the new state.
+const counterReducer = (state = { counter: 0 }, action) => {
+	if (action.type === 'increment') {
+		return {
+			counter: state.counter + 1,
+		};
+	}
+
+	if (action.type === 'decrement') {
+		return {
+			counter: state.counter - 1,
+		};
+	}
+};
+
+// The store is the central hub of Redux.  It holds the current state of the application and will update the state when a specified reducer function is called.
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+	const latestState = store.getState();
+	console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: 'increment' });
