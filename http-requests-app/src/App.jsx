2f0aa52eed7a687064a09a10f80155c087c8bbd0
tx-ares <twinadanz@gmail.com>
2023-12-18 10:38:25 -0600

    Add error handling for updating places

--- http-requests-app/src/App.jsx
+++ http-requests-app/src/App.jsx
@@ -6,12 +6,13 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
 import { updateUserPlaces } from './http.js';
+import Error from './components/Error.jsx';
 
 function App() {
 	const selectedPlace = useRef();
 
 	const [userPlaces, setUserPlaces] = useState([]);
-
+	const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState(null);
 	const [modalIsOpen, setModalIsOpen] = useState(false);
 
 	function handleStartRemovePlace(place) {
@@ -39,7 +40,10 @@ function App() {
 		try {
 			await updateUserPlaces([selectedPlace, ...userPlaces]);
 		} catch (error) {
-			console.error(error);
+			setUserPlaces(userPlaces);
+			setErrorUpdatingPlaces({
+				message: error.message || 'Failed to update places.',
+			});
 		}
 	}
 
@@ -53,8 +57,22 @@ function App() {
 		setModalIsOpen(false);
 	}, []);
 
+	function handleError() {
+		setErrorUpdatingPlaces(null);
+	}
+
 	return (
 		<>
+			<Modal
+				open={errorUpdatingPlaces}
+				onClose={handleError}>
+				{errorUpdatingPlaces && (
+					<Error
+						title='An error occured while updating places.'
+						message={errorUpdatingPlaces.message}
+						onConfirm={handleError}></Error>
+				)}
+			</Modal>
 			<Modal
 				open={modalIsOpen}
 				onClose={handleStopRemovePlace}>
