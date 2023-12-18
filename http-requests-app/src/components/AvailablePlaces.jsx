2ad4fcd00cb3134111d79155ae437657302754e6
tx-ares <twinadanz@gmail.com>
2023-12-18 10:20:30 -0600

    Refactor AvailablePlaces component to handle loading state and display error message

--- http-requests-app/src/components/AvailablePlaces.jsx
+++ http-requests-app/src/components/AvailablePlaces.jsx
@@ -1,26 +1,56 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
 	const [availablePlaces, setAvailablePlaces] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
 
 	useEffect(() => {
-		// fetch() is a browser API for making HTTP requests
-		fetch('http://localhost:3000/places')
-			.then((response) => {
-				// response.json() returns a promise
-				return response.json();
-			})
-			.then((responseData) => {
-				setAvailablePlaces(responseData.places);
-			});
+		setIsLoading(true);
+		async function fetchPlaces() {
+			try {
+				const response = await fetch('http://localhost:3000/places');
+				const responseData = await response.json();
+				if (!response.ok) throw new Error(responseData.message);
+
+				navigator.geolocation.getCurrentPosition(
+					(position) => {
+						const sortedPlaces = sortPlacesByDistance(
+							responseData.places,
+							position.coords.latitude,
+							position.coords.longitude
+						);
+						setAvailablePlaces(sortedPlaces);
+						setIsLoading(false);
+					},
+					(error) => {}
+				);
+			} catch (error) {
+				setError(error);
+				setIsLoading(false);
+			}
+		}
+
+		fetchPlaces();
 	}, []); // Remember that with useEffect, this code only runs if the value of the second argument, ( a dependency changes )
 	// This is kind of like how in Angular we control changeDetection strategies by configuring it in the @Component decorator or using a lifecycle hook like ngOnChanges()
+
+	if (error)
+		return (
+			<Error
+				title='an error has occured!'
+				message={error.message}></Error>
+		);
 
 	return (
 		<Places
 			title='Available Places'
 			places={availablePlaces}
+			isLoading={isLoading}
+			loadingText='Loading places...'
 			fallbackText='No places available.'
 			onSelectPlace={onSelectPlace}
 		/>
