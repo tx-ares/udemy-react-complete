3c71fd133bfac5815008257be4cdcc8945e2f5f1
tx-ares <twinadanz@gmail.com>
2023-12-18 20:50:39 -0600

    update authentication logic , conditionally show auth screen or user profile based on auth state

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,12 +1,16 @@
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+import { useSelector } from 'react-redux';
 
 function App() {
+	const isAuth = useSelector((state) => state.auth.isAuthenticated);
 	return (
 		<>
 			<Header />
-			<Auth />
+			{isAuth && <UserProfile />}
+			{!isAuth && <Auth />}
 			<Counter />
 		</>
 	);
