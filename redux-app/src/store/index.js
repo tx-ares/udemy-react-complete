e0d03d2b2c694f10d128681cc2fadf99ed4f0d86
tx-ares <twinadanz@gmail.com>
2023-12-18 20:08:18 -0600

    Refactor counterReducer in store/index.js

--- redux-app/src/store/index.js
+++ redux-app/src/store/index.js
@@ -8,28 +8,28 @@ const initState = {
 const counterReducer = (state = initState, action) => {
 	if (action.type === 'increment') {
 		return {
+			...state,
 			counter: state.counter + 1,
-			showCounter: state.showCounter,
 		};
 	}
 
 	if (action.type === 'increase') {
 		return {
+			...state,
 			counter: state.counter + action.amount,
-			showCounter: state.showCounter,
 		};
 	}
 
 	if (action.type === 'decrement') {
 		return {
+			...state,
 			counter: state.counter - 1,
-			showCounter: state.showCounter,
 		};
 	}
 
 	if (action.type === 'toggle') {
 		return {
-			counter: state.counter,
+			...state,
 			showCounter: !state.showCounter,
 		};
 	}
