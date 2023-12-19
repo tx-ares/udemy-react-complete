ea3334430289723e24391e32e383b56a65906c84
tx-ares <twinadanz@gmail.com>
2023-12-18 20:17:09 -0600

    Refactor reducer to use reduxtoolkit package

--- redux-app/src/store/index.js
+++ redux-app/src/store/index.js
@@ -1,41 +1,31 @@
-import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initState = {
 	counter: 0,
 	showCounter: true,
 };
 
-const counterReducer = (state = initState, action) => {
-	if (action.type === 'increment') {
-		return {
-			...state,
-			counter: state.counter + 1,
-		};
-	}
+const counterSlice = createSlice({
+	name: 'counter',
+	initialState: initState,
+	reducers: {
+		increment(state) {
+			state.counter++;
+		},
+		decrement(state) {
+			state.counter--;
+		},
+		increase(state, action) {
+			state.counter = state.counter + action.payload.amount;
+		},
+		toggle(state) {
+			state.showCounter = !state.showCounter;
+		},
+	},
+});
 
-	if (action.type === 'increase') {
-		return {
-			...state,
-			counter: state.counter + action.amount,
-		};
-	}
-
-	if (action.type === 'decrement') {
-		return {
-			...state,
-			counter: state.counter - 1,
-		};
-	}
-
-	if (action.type === 'toggle') {
-		return {
-			...state,
-			showCounter: !state.showCounter,
-		};
-	}
-	return state;
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({
+	reducer: counterSlice.reducer,
+});
 
 export default store;
