521d6145804fa69d7e1e7e4028ddebcaaa5b699c
tx-ares <twinadanz@gmail.com>
2024-01-10 08:56:36 -0600

    Refactor User and Users components to use class-based components

--- class-based-components/src/components/User.js
+++ class-based-components/src/components/User.js
@@ -1,7 +1,16 @@
 import classes from './User.module.css';
+import { Component } from 'react';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+// Class based component
+class User extends Component {
+	render() {
+		return <li className={classes.user}>{this.props.name}</li>;
+	}
+}
+
+// Functional component
+// const User = (props) => {
+// 	return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
