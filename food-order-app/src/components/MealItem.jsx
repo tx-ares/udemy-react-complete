b0c797b5ae0f4308f006169c3100af39613cc87c
tx-ares <twinadanz@gmail.com>
2024-01-15 13:04:59 -0600

    Add currency formatting to meal price

--- food-order-app/src/components/MealItem.jsx
+++ food-order-app/src/components/MealItem.jsx
@@ -1,3 +1,5 @@
+import { currencyFormatter } from '../util/formatting';
+
 export default function MealItem({ meal }) {
 	return (
 		<li className='meal-item'>
@@ -8,7 +10,9 @@ export default function MealItem({ meal }) {
 				/>
 				<div>
 					<h3>{meal.name}</h3>
-					<p className='meal-item-price'>{meal.price}</p>
+					<p className='meal-item-price'>
+						{currencyFormatter.format(meal.price)}
+					</p>
 					<p className='meal-item-description'>{meal.description}</p>
 				</div>
 				<p className='meal-item-actions'>
