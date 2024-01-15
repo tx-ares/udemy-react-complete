da0df03366f75464d94aede128908094a871b090
tx-ares <twinadanz@gmail.com>
2024-01-15 12:11:15 -0600

    Add Header component to App.jsx

---
+++ food-order-app/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logoImg from '../assets/logo.jpg';
+
+export default function Header() {
+	return (
+		<header id='main-header'>
+			<div id='title'>
+				<img
+					src={logoImg}
+					alt='restaurant'
+				/>
+				<h1>React Food</h1>
+			</div>
+			<nav>
+				<button>Cart (0)</button>
+			</nav>
+		</header>
+	);
+}
