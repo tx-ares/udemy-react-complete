e7e29da7b8370c6da8ba28c05352a02b3979374f
tx-ares <twinadanz@gmail.com>
2024-01-15 12:27:25 -0600

    Add MealItem and Meals components

---
+++ food-order-app/src/components/Meals.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import MealItem from './MealItem';
+
+export default function Meals() {
+	const [loadedMeals, setloadedMeals] = useState([]);
+
+	useEffect(() => {
+		async function fetchMeals() {
+			const response = await fetch('http://localhost:3000/meals');
+
+			if (!response.ok) {
+				throw new Error('Something went wrong!');
+			}
+
+			const meals = await response.json();
+			setloadedMeals(meals);
+			console.log(loadedMeals);
+		}
+
+		fetchMeals();
+	}, []);
+
+	return (
+		<ul id='meals'>
+			{loadedMeals.map((meal) => (
+				<MealItem meal={meal}></MealItem>
+			))}
+		</ul>
+	);
+}
