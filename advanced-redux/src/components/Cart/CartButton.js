19b1047b3f5a8cf9347da9a611b85372531d2663
tx-ares <twinadanz@gmail.com>
2024-01-22 15:28:32 -0600

    Refactor App.js and CartButton.js

--- advanced-redux/src/components/Cart/CartButton.js
+++ advanced-redux/src/components/Cart/CartButton.js
@@ -1,12 +1,22 @@
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
-  return (
-    <button className={classes.button}>
-      <span>My Cart</span>
-      <span className={classes.badge}>1</span>
-    </button>
-  );
+	const dispatch = useDispatch();
+
+	const toggleCartHandler = () => {
+		dispatch(uiActions.toggle());
+	};
+
+	return (
+		<button
+			className={classes.button}
+			onClick={toggleCartHandler}>
+			<span>My Cart</span>
+			<span className={classes.badge}>1</span>
+		</button>
+	);
 };
 
 export default CartButton;
