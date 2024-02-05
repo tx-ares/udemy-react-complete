5bc9f544aa89ac924e59fd9e54ee1a23664c406d
tx-ares <twinadanz@gmail.com>
2024-02-05 16:49:29 -0600

    add dummy assets

--- advanced-redux/src/components/Shop/Products.js
+++ advanced-redux/src/components/Shop/Products.js
@@ -1,19 +1,24 @@
+import { DUMMY_PRODUCTS } from '../../dummy-products.js';
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+	return (
+		<section className={classes.products}>
+			<h2>Buy your favorite products</h2>
+			<ul>
+				{DUMMY_PRODUCTS.map((product) => (
+					<ProductItem
+						key={product.id}
+						id={product.id}
+						title={product.title}
+						price={product.price}
+						description={product.description}
+					/>
+				))}
+			</ul>
+		</section>
+	);
 };
 
 export default Products;
